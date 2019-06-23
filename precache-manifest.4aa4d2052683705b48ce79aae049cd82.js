ef12290d32ec3971a78cac973b22df13e9c110d4
EminQasimov <eminqasimov@outlook.com>
2019-06-23 16:11:30 +0400

    Updates

---
+++ precache-manifest.4aa4d2052683705b48ce79aae049cd82.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49fae269ce15e7f24a772c0cc0216a0f",
+    "url": "/hospital-dashboard-with-antdui/index.html"
+  },
+  {
+    "revision": "ff93580b6e5e39f7aa81",
+    "url": "/hospital-dashboard-with-antdui/static/css/2.ad5be608.chunk.css"
+  },
+  {
+    "revision": "2dcf3aaa5258d74495ed",
+    "url": "/hospital-dashboard-with-antdui/static/css/main.aba494c9.chunk.css"
+  },
+  {
+    "revision": "ff93580b6e5e39f7aa81",
+    "url": "/hospital-dashboard-with-antdui/static/js/2.7131ccea.chunk.js"
+  },
+  {
+    "revision": "2dcf3aaa5258d74495ed",
+    "url": "/hospital-dashboard-with-antdui/static/js/main.e28ed630.chunk.js"
+  },
+  {
+    "revision": "54798ea18a8b797bec99",
+    "url": "/hospital-dashboard-with-antdui/static/js/runtime~main.2ac1dcb8.js"
+  },
+  {
+    "revision": "2737490d0d474ac53ef282e0d51beb61",
+    "url": "/hospital-dashboard-with-antdui/static/media/capsul.2737490d.svg"
+  },
+  {
+    "revision": "299f4d5aafd524798f61889e5b08d15c",
+    "url": "/hospital-dashboard-with-antdui/static/media/doctor.299f4d5a.svg"
+  },
+  {
+    "revision": "ed2dc50ce0f692792fa3f35c2f48a0b8",
+    "url": "/hospital-dashboard-with-antdui/static/media/dollar.ed2dc50c.svg"
+  },
+  {
+    "revision": "c2a12a077cec6963216ea5e9360499a5",
+    "url": "/hospital-dashboard-with-antdui/static/media/feet.c2a12a07.svg"
+  },
+  {
+    "revision": "7e1694ac11280d0f60cb1dd2a501e32d",
+    "url": "/hospital-dashboard-with-antdui/static/media/heart.7e1694ac.svg"
+  },
+  {
+    "revision": "00961dd1c42379131f029fae48a75baa",
+    "url": "/hospital-dashboard-with-antdui/static/media/laboratorst.00961dd1.svg"
+  },
+  {
+    "revision": "66f3a19c223e003e96da9a4454cdd493",
+    "url": "/hospital-dashboard-with-antdui/static/media/logo.66f3a19c.svg"
+  },
+  {
+    "revision": "7c7c47f437890d0280996b1c8d241a7b",
+    "url": "/hospital-dashboard-with-antdui/static/media/needle.7c7c47f4.svg"
+  },
+  {
+    "revision": "7b05130d145a2c9eb6a52dfa33836711",
+    "url": "/hospital-dashboard-with-antdui/static/media/operation.7b05130d.svg"
+  },
+  {
+    "revision": "61e41b0f34317581e685ae49d938fed7",
+    "url": "/hospital-dashboard-with-antdui/static/media/rip.61e41b0f.svg"
+  }
+]);
